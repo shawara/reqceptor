cd5891502bbe96170c4e29ad129ade8baaed17f2
Mahmoud Shawara <shawara@users.noreply.github.com>
2025-06-15 01:54:26 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const config = {
-  apiUrl: isDevelopment ? 'http://localhost:3001' : 'https://webhook-interceptor.onrender.com',
-  socketUrl: isDevelopment ? 'http://localhost:3001' : 'https://webhook-interceptor.onrender.com',
+  apiUrl: isDevelopment ? 'http://localhost:3001' : 'https://api.reqceptor.shawara.me',
+  socketUrl: isDevelopment ? 'http://localhost:3001' : 'https://api.reqceptor.shawara.me',
   basePath: '' // Removed basePath since we're using a custom domain
 }; 
